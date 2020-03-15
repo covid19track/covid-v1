92a3c63f452e246e7a3efe9e4dc85de8c9b7c204
Edward Pasenidis <edwardp@thecodinglab.tk>
2020-03-15 13:19:21 +0200

    fix (not ready): countries display list

--- public/js/country.js
+++ public/js/country.js
@@ -1,18 +1,24 @@
-let countries = document.getElementById('countries');
+function listappend(div, adata, num) {
+    var par = document.createElement("p");
+    par.appendChild(document.createTextNode(adata));
+    par.setAttribute("id", `cdiv${num}`);
+    $(div).prepend(`<p>${adata} <br>`);
+}
 
-function appendText(inserted) {
-    var txt = document.createElement("p");
-    txt.innerHTML = inserted;         // Create text with DOM
-    $("body").append(txt);   // Append new elements
-  }
 
-fetch('https://coronavirus-19-api.herokuapp.com/countries')
-    .then((resp) => resp.json())
-    .then(function (data) {
-        for (let i = 0; i < data.length; i++) {;
-            appendText(`Country: ${data[i].country} <br> Cases: ${data[i].cases} <br> Recovered ${data[i].recovered}`)
-        }
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+function refresh() {
+
+    fetch('https://coronavirus-19-api.herokuapp.com/countries')
+        .then((resp) => resp.json())
+        .then(function (data) {
+            for (let i = 0; i < data.length; i++) {
+                ul = document.createElement("div");
+                ul.setAttribute('id', `cdiv${i}`);
+                listappend(ul, data[i].country, i);
+                // appendText(`Country: ${data[i].country} <br> Cases: ${data[i].cases} <br> Recovered ${data[i].recovered}`);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
