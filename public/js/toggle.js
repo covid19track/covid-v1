7b88eac8b57e1f7dacb54125bef831455393dee0
Edward Pasenidis <edwardp@thecodinglab.tk>
2020-03-15 11:58:14 +0200

    add: dark mode switch as a seperate file

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -7,7 +7,9 @@ function dark() {
                 brieftext.style.color = 'rgb(255, 255, 255)';
                 button.style.color = '#333';
                 button.style.backgroundColor = 'rgb(255, 255, 255)';
+                categ.style.color = 'rgb(255, 255, 255)';
                 document.getElementById('switcher').innerHTML = "Light Mode";
+                document.getElementsByTagName('ul').style.backgroundColor = 'rgb(255, 255, 255';
         }
         else {
                 document.body.style.backgroundColor = 'rgb(255, 255, 255)';
@@ -16,6 +18,8 @@ function dark() {
                 brieftext.style.color = '#333';
                 button.style.color = 'rgb(255, 255, 255)';
                 button.style.backgroundColor = '#333';
+                categ.style.color('#333');
                 document.getElementById('switcher').innerHTML = "Dark Mode";
+                document.getElementsByTagName('ul').style.backgroundColor = '#333';
         }
 }
