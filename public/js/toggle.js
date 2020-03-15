910206982474024a23bf28b0439dbad9e54f672b
Edward Pasenidis <edwardp@thecodinglab.tk>
2020-03-15 13:18:52 +0200

    fix: navigation menu

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,3 +1,5 @@
+var button = document.getElementById('switcher');
+
 function dark() {
         if (document.body.style.backgroundColor == 'rgb(255, 255, 255)') {
 
@@ -7,9 +9,9 @@ function dark() {
                 brieftext.style.color = 'rgb(255, 255, 255)';
                 button.style.color = '#333';
                 button.style.backgroundColor = 'rgb(255, 255, 255)';
-                categ.style.color = 'rgb(255, 255, 255)';
+                // categ.style.color = 'rgb(255, 255, 255)';
                 document.getElementById('switcher').innerHTML = "Light Mode";
-                document.getElementsByTagName('ul').style.backgroundColor = 'rgb(255, 255, 255';
+                // document.getElementsByTagName('ul').style.backgroundColor = 'rgb(255, 255, 255';
         }
         else {
                 document.body.style.backgroundColor = 'rgb(255, 255, 255)';
@@ -18,8 +20,8 @@ function dark() {
                 brieftext.style.color = '#333';
                 button.style.color = 'rgb(255, 255, 255)';
                 button.style.backgroundColor = '#333';
-                categ.style.color('#333');
+                // categ.style.color('#333');
                 document.getElementById('switcher').innerHTML = "Dark Mode";
-                document.getElementsByTagName('ul').style.backgroundColor = '#333';
+                // document.getElementsByTagName('ul').style.backgroundColor = '#333';
         }
 }
