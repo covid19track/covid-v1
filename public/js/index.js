0ba030e75747c1ca294b0c88ca39600aa997366c
Edward Pasenidis <edwardp@thecodinglab.tk>
2020-03-19 21:53:56 +0200

    Update: Better chart display

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
-var ctx = document.getElementById('covid').getContext('2d');
-var title = document.getElementById('title');
-var subtitle = document.getElementById('subtitle');
-var button = document.getElementById('switcher');
-var link = document.getElementById('categ');
+let ctx = document.getElementById('covid').getContext('2d');
+let title = document.getElementById('title');
+let subtitle = document.getElementById('subtitle');
+let button = document.getElementById('switcher');
+let link = document.getElementById('categ');
 
 let graphcountries = [];
 let graphcases = [];
@@ -15,28 +15,53 @@ fetch('https://coronavirus-19-api.herokuapp.com/countries')
             graphcountries[i] = data[i].country;
             graphcases[i] = data[i].cases;
         }
+
+        let covid_chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: graphcountries,
+                datasets: [{
+                    label: 'Top 10 Countries',
+                    data: graphcases,
+                    backgroundColor: [
+                        'rgba(360, 0, 0, 0.7)', // 1
+                        'rgba(325, 0, 0, 0.7)', // 2
+                        'rgba(300, 0, 0, 0.7)', // 3
+                        'rgba(270, 0, 0, 0.7)', // 4
+                        'rgba(255, 0, 0, 0.7)', // 5 
+                        'rgba(230, 0, 0, 0.7)', // 6 
+                        'rgba(200, 0, 0, 0.7)', // 7
+                        'rgba(150, 0, 0, 0.7)', // 8
+                        'rgba(102, 0, 0, 0.7)', // 9
+                        'rgba(77, 0, 0, 0.7)' // 10
+                    ],
+                    borderColor: [
+                        'rgba(77, 0, 0, 0.7)', // 1
+                        'rgba(102, 0, 0, 0.7)', // 2
+                        'rgba(150, 0, 0, 0.7)', // 3
+                        'rgba(200, 0, 0, 0.7)', // 4
+                        'rgba(230, 0, 0, 0.7)', // 5 
+                        'rgba(255, 0, 0, 0.7)', // 6 
+                        'rgba(270, 0, 0, 0.7)', // 7
+                        'rgba(300, 0, 0, 0.7)', // 8
+                        'rgba(325, 0, 0, 0.7)', // 9
+                        'rgba(360, 0, 0, 0.7)', // 10
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
     })
+
     .catch(function (error) {
         console.log(error);
     })
-
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-        labels: graphcountries,
-        datasets: [{
-            label: 'Top 10 Countries',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: graphcases
-        }]
-    },
-
-    // Configuration options go here
-    options: {
-        responsive: true
-    }
-});
