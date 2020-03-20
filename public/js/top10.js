2241261df5b30cb4697b01fbf5750bbb711b5d81
unknown <gustavolama20@gmail.com>
2020-03-21 01:36:22 +0200

    Fixed identation

--- public/js/top10.js
+++ public/js/top10.js
@@ -1,75 +1,69 @@
-console.log('Top10.JS Initialization');
+console.log('top10.js Initialization');
 
-let ctx       = document.getElementById('covid').getContext('2d');
-let title     = document.getElementById('title');
-let subtitle  = document.getElementById('subtitle');
-let button    = document.getElementById('switcher');
-let link      = document.getElementById('categ');
+let ctx = document.getElementById('covid').getContext('2d');
 
-console.log(`Canvas found: ${ctx}`);
+//console.log(`Canvas found: ${ctx}`);
 
 let graphcountries = [];
 let graphcases = [];
 
-function reload() {
+$(document).ready(() => {
 
-    fetch('https://coronavirus-19-api.herokuapp.com/countries')
-        .then((resp) => resp.json())
-        .then(function (data) {
+  fetch('https://coronavirus-19-api.herokuapp.com/countries')
+    .then(resp => resp.json())
+    .then(data => {
 
-            for (let i = 0; i < 10; i++) {
-                graphcountries[i] = data[i].country;
-                graphcases[i] = data[i].cases;
-            }
+      for (let i = 0; i < 10; i++) {
+        graphcountries[i] = data[i].country;
+        graphcases[i] = data[i].cases;
+      }
 
-            let covid_chart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: graphcountries,
-                    datasets: [{
-                        label: 'Top 10 Countries',
-                        data: graphcases,
-                        backgroundColor: [
-                            'rgba(360, 0, 0, 0.7)', // 1
-                            'rgba(325, 0, 0, 0.7)', // 2
-                            'rgba(300, 0, 0, 0.7)', // 3
-                            'rgba(270, 0, 0, 0.7)', // 4
-                            'rgba(255, 0, 0, 0.7)', // 5
-                            'rgba(230, 0, 0, 0.7)', // 6
-                            'rgba(200, 0, 0, 0.7)', // 7
-                            'rgba(150, 0, 0, 0.7)', // 8
-                            'rgba(102, 0, 0, 0.7)', // 9
-                            'rgba(77, 0, 0, 0.7)' // 10
-                        ],
-                        borderColor: [ // borders
-                            'rgba(77, 0, 0, 0.7)', // 1
-                            'rgba(102, 0, 0, 0.7)', // 2
-                            'rgba(150, 0, 0, 0.7)', // 3
-                            'rgba(200, 0, 0, 0.7)', // 4
-                            'rgba(230, 0, 0, 0.7)', // 5
-                            'rgba(255, 0, 0, 0.7)', // 6
-                            'rgba(270, 0, 0, 0.7)', // 7
-                            'rgba(300, 0, 0, 0.7)', // 8
-                            'rgba(325, 0, 0, 0.7)', // 9
-                            'rgba(360, 0, 0, 0.7)', // 10
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: { // options
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    },
-                    responsive: true
+      let covid_chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: graphcountries,
+          datasets: [{
+            label: 'Cases Per Country',
+            data: graphcases,
+              backgroundColor: [
+                'rgba(360, 0, 0, 0.7)', // 1
+                'rgba(325, 0, 0, 0.7)', // 2
+                'rgba(300, 0, 0, 0.7)', // 3
+                'rgba(270, 0, 0, 0.7)', // 4
+                'rgba(255, 0, 0, 0.7)', // 5
+                'rgba(230, 0, 0, 0.7)', // 6
+                'rgba(200, 0, 0, 0.7)', // 7
+                'rgba(150, 0, 0, 0.7)', // 8
+                'rgba(102, 0, 0, 0.7)', // 9
+                'rgba(77, 0, 0, 0.7)' // 10
+              ],
+              borderColor: [ // borders
+                'rgba(77, 0, 0, 0.7)', // 1
+                'rgba(102, 0, 0, 0.7)', // 2
+                'rgba(150, 0, 0, 0.7)', // 3
+                'rgba(200, 0, 0, 0.7)', // 4
+                'rgba(230, 0, 0, 0.7)', // 5
+                'rgba(255, 0, 0, 0.7)', // 6
+                'rgba(270, 0, 0, 0.7)', // 7
+                'rgba(300, 0, 0, 0.7)', // 8
+                'rgba(325, 0, 0, 0.7)', // 9
+                'rgba(360, 0, 0, 0.7)', // 10
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: { // options
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
                 }
-            });
-        })
-
-        .catch(function (error) {
-            console.log(error);
-        })
-}
+              }]
+            },
+          responsive: true
+        }
+      });
+    }).catch(error => {
+      console.log("Chart.js Error:", error);
+    });
+});
