e4c278e53e0797f6238ed9a0360a109c5099846e
unknown <gustavolama20@gmail.com>
2020-03-21 15:51:49 +0200

    Navbar responsivness fix

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,9 @@
 console.log('main.JS initialization');
+
+//navbar responsiveness
+$(document).ready(() => {
+  $(".navbar-burger").click(() => {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+});
