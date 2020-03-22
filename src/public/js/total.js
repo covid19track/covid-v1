e1a59f6e73d0d296b2d207a6eda5e5d811ee63ac
unknown <gustavolama20@gmail.com>
2020-03-23 00:20:07 +0200

    update: added total countries chart for cases

--- src/public/js/total.js
+++ src/public/js/total.js
@@ -1,5 +1,73 @@
 console.log('total.js initialization');
 
-function getTotalData() {
+function getTotalData(countryNumber) {
+  fetch('https://coronavirus-19-api.herokuapp.com/countries')
+      .then((resp) => resp.json())
+      .then((data) => {
+
+          $('.country-number').html(`${countryNumber}/${data.length}`)
+          $('#slider').attr('max', data.length);
+
+          let countries = [];
+          let countryLabels = [];
+          let countryCases = [];
+
+          for (let i = 0; i < countryNumber; i++) {
+            countries.push(data[i]);
+            countryLabels.push(data[i].country);
+            countryCases.push(data[i].cases);
+          }
+
+          let casesCtx = document.getElementById('cases_canvas').getContext('2d');
+
+          if (window.bar) window.bar.destroy();
+
+          window.bar = new Chart(casesCtx, {
+            type: 'bar',
+            data: {
+              labels: countryLabels,
+              datasets: [{
+                label: 'Cases Per Country',
+                data: countryCases,
+                backgroundColor: 'rgba(255, 0, 0, 0.7)'
+              }],
+            },
+            options: {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true,
+                  },
+                }],
+              },
+              responsive: true,
+            },
+          });
+
+      }).catch(() => {
+        console.log('Request Error');
+      });
 
 }
+
+$('.slider').on('input', () => {
+  getTotalData($('.slider').val())
+});
+
+$('.refresh-btn').click(() => {
+  $('.refresh-btn > i').css({
+    'transition-duration': '1.5s',
+    'transform': 'rotate(360deg)',
+  });
+  $('.refresh-btn > i').bind('transitionend', () => {
+    $('.refresh-btn > i').css({
+      'transition-duration': '',
+      'transform': '',
+    });
+  });
+  getTotalData($('.slider').val())
+});
+
+$(document).ready(() => {
+  getTotalData(10);
+});
