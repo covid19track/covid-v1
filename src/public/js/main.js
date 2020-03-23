ff67cfbedc4d375b67d0f3d421b7d13918e670fb
unknown <gustavolama20@gmail.com>
2020-03-23 21:39:01 +0200

    styling: responsive subheader

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,4 +5,10 @@ $(document).ready(() => {
     $('.navbar-burger').toggleClass('is-active');
     $('.navbar-menu').toggleClass('is-active');
   });
+  if ($(window).width() <= 510) {
+    $('#header').removeClass('is-size-1');
+    $('#header').addClass('is-size-3');
+    $('.subheader').removeClass('is-size-2');
+    $('.subheader').addClass('is-size-4');
+  }
 });
